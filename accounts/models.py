c1817e2e858cf1c1e866b1cd89aec9c58c0619f5
gabriel2302 <gabriel23002@gmail.com>
2019-12-04 22:50:10 -0300

    Último Commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from contatos.models import Contato
 from django import forms
+from django import forms
 
 
 class FormContato(forms.ModelForm):
+
     class Meta:
         model = Contato
         exclude = ('mostrar',)
